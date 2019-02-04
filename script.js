ba77e7ec182a8aad49d665fe11cfe968b5510555
Jasmin Vargas <jas.vrgs@gmail.com>
2019-02-04 17:05:41 -0500

    added grid to calc;attempted to do it with bootstrap but went back to original way

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log ("hello world");
+//Create grid for calc
+    //4 col x 6 rows
+    //blow up boxes
+    //add numbers inside
+    //change background
+    //figure out how to merge "0" button
+
+
+//add function to buttons
+    //clear button reset 
